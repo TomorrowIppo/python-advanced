1e8455381bdae66a448a18dd8681157133794159
tomorrowippo <bjk1624@gmail.com>
2026-04-11 13:25:20 +0900

    feat: kau_ps_week6 6주차 백준 완료

--- Algorithm/ps.py
+++ Algorithm/ps.py
@@ -2,21 +2,20 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-pattern = input().strip()
+inp = list(input().strip())
+stack = []
 
-prefix, suffix = pattern.split('*')
-p_len = len(prefix)
-s_len = len(suffix)
-
-for _ in range(n):
-    inp_str = input().strip()
-
-    if len(inp_str) < (p_len + s_len):
-        print("NE")
-        continue
-
-    if inp_str[:p_len] == prefix and inp_str[-s_len:] == suffix:
-        print("DA")
+for c in inp:
+    if c == ')':
+        if stack and stack[-1] == '(':
+            stack.pop()
+        else:
+            stack.append(c)
     else:
-        print("NE")
+        stack.append(c)
+    
+cnt = 0
+while stack:
+    stack.pop()
+    cnt += 1
+print(cnt)
