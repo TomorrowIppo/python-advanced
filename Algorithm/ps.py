c939e4bef2e3657ead37f33c6775d054b619f963
TomorrowIppo <bjk1624@gmail.com>
2026-04-12 00:06:59 +0900

    feat: kau_ps_week7 7주차 백준 완료

--- Algorithm/ps.py
+++ Algorithm/ps.py
@@ -1,21 +1,54 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-inp = list(input().strip())
-stack = []
 
-for c in inp:
-    if c == ')':
-        if stack and stack[-1] == '(':
-            stack.pop()
-        else:
-            stack.append(c)
-    else:
-        stack.append(c)
-    
-cnt = 0
-while stack:
-    stack.pop()
-    cnt += 1
-print(cnt)
+# 전처리
+M, N = map(int, input().split())
+
+board = [input().strip() for _ in range(M)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+vis = [[False for _ in range(N)] for _ in range(M)]
+
+#print(board)
+
+
+# BFS
+def BFS(x, y):
+    global board, dx, dy
+
+    vis[x][y] = True
+
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        cur_x, cur_y = q.popleft()
+
+        for dir in range(4):
+            nx, ny = (cur_x + dx[dir], cur_y + dy[dir])
+
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                continue
+            if vis[nx][ny] or board[nx][ny] != '0':
+                continue
+
+            vis[nx][ny] = True
+            q.append((nx, ny))
+
+
+# Flood-Fill
+for i in range(N):
+    if board[0][i] == '0':
+        BFS(0, i)
+
+for i in range(N):
+    if vis[M - 1][i]:
+        print("YES")
+        exit()
+
+print("NO")
+        
+
